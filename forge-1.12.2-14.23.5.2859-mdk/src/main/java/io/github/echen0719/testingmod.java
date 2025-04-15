6c768f0a0282d3f96866cd82a904c3cedb129d7b
Eric Chen <echen0719@gmail.com>
2025-04-14 20:06:57 -0400

    Current Progress
    slow but steady

---
+++ forge-1.12.2-14.23.5.2859-mdk/src/main/java/io/github/echen0719/testingmod.java
@@ -0,0 +1,31 @@
+package io.github.echen0719;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = testingmod.MODID, name = testingmod.NAME, version = testingmod.VERSION, useMetadata = true)
+public class testingmod // and lowercase for some reason
+{
+    public static final String MODID = "testingmod"; // has to be same as class
+    public static final String NAME = "echen0719's Test Mod";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
